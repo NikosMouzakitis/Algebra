07d1bb650972632d61164d06993538392a19a9f0
NikosMouzakitis <mzktsn@gmail.com>
2016-04-11 21:21:17 +0300

    Update prime_properties.c

--- prime_properties.c
+++ prime_properties.c
@@ -1,3 +1,9 @@
+
+/* "Prime Numbers And Properties"
+   Written by Mouzakitis Nikolaos
+  ,April 2016
+*/
+
 #include <stdio.h>
 #include "biblioteca.h"
 #include <stdlib.h>
@@ -6,14 +12,15 @@
 int main(int argc,char *argv[])
 {
 
-        int N,cnt,array_cnt = 0;
+        int combo,N,cnt,array_cnt = 0;
         printf("Enter your search range.\n");
         scanf("%d",&N);
         int prarray[N] ;
 
         printf("Program presenting properties on prime numbers.\n");
         printf("*Dist.ToNext-the distance to the next prime number.\n");
-        printf("**HwManyTimes(2)- the integer part of the prime divided by 2.\n");
+        printf("*HwManyTimes(2)- the integer part of the prime divided by 2.\n");
+        printf("*combo1 - Checks if the sum of two primes minus 1 is a prime number.\n");
         printf("\n");
 
         prarray[N-1] = 5;
@@ -32,8 +39,31 @@ int main(int argc,char *argv[])
         //printing the results on screen.
         printf("Prime\tDist.ToNext\tHwManyTimes(2)\n");
         for(cnt = 0 ; cnt < N; cnt ++)
-                printf("%d\t  %d\t\t%d\n",prarray[cnt],prarray[cnt+1] - prarray[cnt],(int)prarray[cnt]/2);
+        {
+
+                if (cnt == N-1)
+                        printf("%d\t    \t\t%d\n",prarray[cnt],(int)prarray[cnt]/2);
+                else
+                        printf("%d\t  %d\t\t%d\n",prarray[cnt],prarray[cnt+1] - prarray[cnt],(int)prarray[cnt]/2);
+        }
+
+        printf("\n");
+        printf("Combo 1 search results.\n\n");
+
+        for(cnt = 0; cnt < N ; cnt ++)
+        {
+                combo = combo1(prarray[cnt],prarray[1]);
+                if(combo)
+                        printf("%d , %d resulting prime %d \n",prarray[cnt],prarray[1],prarray[cnt] + prarray[1] -1);
+        }
+
+
+
+
+
+
 
 
         return (0);
 }
+
